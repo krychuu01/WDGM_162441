b20411ea8047584a2cbbb8fda39643fbd227cbe1
GKrych <hazzidy125@gmail.com>
2022-10-14 12:02:33 +0200

    adding unversioned files

---
+++ cwiczenia/cw_Test.py
@@ -0,0 +1,11 @@
+from matplotlib.image import imread
+from matplotlib.pyplot import imshow
+
+from WDGM_162441.cwiczenia.cw_2.BaseImage import BaseImage
+
+img_arr = BaseImage('lena.jpg')
+
+# print(img_arr)
+
+type(img_arr)
+
