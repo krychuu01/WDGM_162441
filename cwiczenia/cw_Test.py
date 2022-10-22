c6b107706b75ff92e65796715dec33214d06855f
GKrych <hazzidy125@gmail.com>
2022-10-22 14:53:45 +0200

    changed to_hsv() method to use numpy

--- cwiczenia/cw_Test.py
+++ cwiczenia/cw_Test.py
@@ -1,11 +1,25 @@
-from matplotlib.image import imread
-from matplotlib.pyplot import imshow
-
+from WDGM_162441.cwiczenia.cw_2 import ColorModel
 from WDGM_162441.cwiczenia.cw_2.BaseImage import BaseImage
+import matplotlib.pyplot as plt
+from matplotlib.colors import hsv_to_rgb
 
-img_arr = BaseImage('lena.jpg')
+img1 = BaseImage('lena.jpg', color_model=ColorModel.ColorModel.rgb)
+# img2 = BaseImage('lena.jpg', color_model=ColorModel.ColorModel.rgb)
+# img3 = BaseImage('lena.jpg', color_model=ColorModel.ColorModel.rgb)
 
-# print(img_arr)
+hsv = img1.to_hsv()
+# hsv.show_as_rgb_layers()
+# hsv.show_img_without_axis()
+# hsv.show_as_rgb_layers()
+hsv.to_rgb()
+# img2.to_hsi()
+# img3.to_hsl()
 
-type(img_arr)
-
+hsv.show_img_without_axis()
+hsv.show_as_rgb_layers()
+# img1.show_as_rgb_layers()
+# hsv.show_img()
+# hsv.show_img_without_axis()
+# img1.show_img_without_axis()
+# img2.show_img_without_axis()
+# img3.show_img_without_axis()
